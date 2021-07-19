9fd3cb0ef8b976ca80e71594774a3230a6ffc3c4
Satu Biswas <satubu51@gmail.com>
2021-07-19 23:26:05 +0600

    init

---
+++ src/main/java/com/git/test/gittest/GittestApplication.java
@@ -0,0 +1,13 @@
+package com.git.test.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittestApplication.class, args);
+    }
+
+}
